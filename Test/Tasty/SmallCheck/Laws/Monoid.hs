015f9b8abdf102314caf1cc3eda030a2d1b95d1f
Danny Navarro <j@dannynavarro.net>
2015-07-07 11:41:03 +0200

    Golfing

--- Test/Tasty/SmallCheck/Laws/Monoid.hs
+++ Test/Tasty/SmallCheck/Laws/Monoid.hs
@@ -5,16 +5,16 @@ module Test.Tasty.SmallCheck.Laws.Monoid where
 #if !MIN_VERSION_base(4,8,0)
 import Data.Monoid (Monoid)
 #endif
-import Test.SmallCheck.Series (Serial, Series)
+import Test.SmallCheck.Series (Series)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.SmallCheck (testProperty)
 
 import Test.SmallCheck.Laws.Monoid
 
-testMonoidLaws :: (Show a, Eq a, Monoid a, Serial IO a) => Series IO a -> TestTree
-testMonoidLaws s = testGroup "Monoid laws"
-  [ testProperty "mempty <> x = x" $ leftIdentity s
-  , testProperty "x <> mempty = x" $ rightIdentity s
-  , testProperty "x <> (y <> z) = (x <> y) <> z" $ associativity s
-  , testProperty "mconcat = foldr mappend mempty" $ mconcatProp s
+testMonoid :: (Show a, Eq a, Monoid a) => Series IO a -> TestTree
+testMonoid s = testGroup "Monoid laws" $ map ($ s)
+  [ testProperty "mempty <> x = x" . leftIdentity
+  , testProperty "x <> mempty = x" . rightIdentity
+  , testProperty "x <> (y <> z) = (x <> y) <> z" . associativity
+  , testProperty "mconcat = foldr mappend mempty" . mconcatProp
   ]
