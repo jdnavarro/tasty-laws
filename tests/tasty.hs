b20fc9667f4bcba0dca8782a53bb2e4e09f8ca07
Danny Navarro <j@dannynavarro.net>
2015-07-16 12:00:17 +0200

    Add more Monoid laws tests

--- tests/tasty.hs
+++ tests/tasty.hs
@@ -6,7 +6,7 @@ module Main where
 #if !MIN_VERSION_base(4,8,0)
 import Control.Applicative ((<$>))
 #endif
-import Data.Monoid (Sum(..))
+import Data.Monoid (Sum(..), Product(..))
 
 import Test.SmallCheck.Series (Serial(series), Series)
 import Test.Tasty (defaultMain, testGroup)
@@ -18,5 +18,17 @@ main = defaultMain $ testGroup "Monoid"
   [ testGroup "Sum"
     [ testGroup "Int"
       [ testMonoid $ Sum <$> (series :: Series IO Int) ]
+    , testGroup "Integer"
+      [ testMonoid $ Sum <$> (series :: Series IO Integer) ]
+    , testGroup "Float"
+      [ testMonoid $ Sum <$> (series :: Series IO Float) ]
+    ]
+  , testGroup "Product"
+    [ testGroup "Int"
+      [ testMonoid $ Product <$> (series :: Series IO Int) ]
+    , testGroup "Integer"
+      [ testMonoid $ Product <$> (series :: Series IO Integer) ]
+    , testGroup "Float"
+      [ testMonoid $ Product <$> (series :: Series IO Float) ]
     ]
   ]
